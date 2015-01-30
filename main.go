c545216783e90c27d018610103082ba193a0e947
范杨 <fanyang.coder@gmail.com>
2015-01-31 00:01:40 +0800

    change by logger and config

--- main.go
+++ main.go
@@ -6,17 +6,9 @@ import (
     "github.com/fanyang1988/gologger"
 )
 
-type MainController struct {
-    beego.Controller
-}
-
-func (this *MainController) Get() {
-    this.Ctx.WriteString("hello world")
-}
-
 var (
-    ConfigManager = goconfig.NewConfig()
-    LogManager    = log.NewLog("logger", "./config/log.json", ConfigManager)
+    ConfigManager = goconfig.New()
+    LogManager    = gologger.New("logger", "./config/log.json", ConfigManager)
 )
 
 func init() {
@@ -26,9 +18,6 @@ func main() {
     defer ConfigManager.Close()
     defer LogManager.Close()
 
-    server_info := &ServerInfoController{}
-
-    beego.Router("/", &MainController{})
-    beego.Router("/servers", server_info)
+    beego.Router("/servers", &ServerInfoController{})
     beego.Run("0.0.0.0:7928")
 }
